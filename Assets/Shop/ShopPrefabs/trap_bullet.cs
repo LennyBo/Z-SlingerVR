6e2d460ff46aab6ce4adaf12d611c1c3203577b7
Lenny Boegli <lenny.boegli@he-arc.ch>
2021-09-06 13:40:43 +0200

    Merge

--- Assets/Shop/ShopPrefabs/trap_bullet.cs
+++ Assets/Shop/ShopPrefabs/trap_bullet.cs
@@ -12,53 +12,58 @@ public class trap_bullet : MonoBehaviour
     [SerializeField] private float SHOOT_PER_SEC;
 
     private bool canShoot = false;
-    // Start is called before the first frame update
-    void Start()
-    {
+    Vector3 relativePos;
+    private Vector3 relativeOr;
+
+    private bool isGrabbed = false;
+    public bool IsGrabbed {
+        get { return isGrabbed; }
+        set {
+            if (value) {
+                canShoot = false;
+            }
+            isGrabbed = value;
+        }
     }
     
     public LayerMask stickableItems;
 
     void OnDrawGizmos()
-        {
-            // Draw a yellow sphere at the transform's position
-            Gizmos.color = Color.red;
-            //Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
-            //Gizmos.matrix = rotationMatrix;
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.red;
+        //Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+        //Gizmos.matrix = rotationMatrix;
 
-            float y = 0.05f;
-            GameObject go = new GameObject();
-            Transform f = go.transform;
-            f.position = transform.position;
-            f.rotation = transform.rotation;
+        float y = 0.05f;
+        GameObject go = new GameObject();
+        Transform f = go.transform;
+        f.position = transform.position;
+        f.rotation = transform.rotation;
 
-            f.Translate(new Vector3(0, -y, 0), Space.Self);
-            Vector3 scale = new Vector3(transform.lossyScale.x, y, transform.lossyScale.z);
-            Gizmos.DrawWireSphere(f.position, y);
-            bool b = Physics.CheckSphere(f.position, y, stickableItems);
-            DestroyImmediate(go);
-            Debug.Log(b);
-        }
-        
+        f.Translate(new Vector3(0, -y, 0), Space.Self);
+        Vector3 scale = new Vector3(transform.lossyScale.x, y, transform.lossyScale.z);
+        Gizmos.DrawWireSphere(f.position, y);
+        bool b = Physics.CheckSphere(f.position, y, stickableItems);
+        DestroyImmediate(go);
+        //Debug.Log(b);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (isGrabbed)
+            return;
+        
         if (!canShoot) {
-
-            float y = 0.05f;
-            GameObject go = new GameObject();
-            Transform f = go.transform;
-            f.position = transform.position;
-            f.rotation = transform.rotation;
-
-            f.Translate(new Vector3(0, -y, 0), Space.Self);
-            Vector3 scale = new Vector3(transform.lossyScale.x, y, transform.lossyScale.z);
-            Collider[] colliders = Physics.OverlapSphere(f.position, y, stickableItems);
-            if (colliders.Length >= 1) {
-                transform.SetParent(colliders[0].GetComponent<Transform>());
-            }
+            TryToPlace();
             return;
         }
+        
+        // only for debug purposes
+        //transform.localPosition = relativePos;
+        //transform.localEulerAngles = relativeOr;
+        
         if (shootCounter * SHOOT_PER_SEC >= 1) {
             Shoot();
             shootCounter = 0;
@@ -67,7 +72,33 @@ public class trap_bullet : MonoBehaviour
         }
     }
 
-    void Shoot()
+    private void TryToPlace() {
+        float y = 0.05f;
+        GameObject go = new GameObject();
+        Transform f = go.transform;
+        f.position = transform.position;
+        f.rotation = transform.rotation;
+
+        f.Translate(new Vector3(0, -y, 0), Space.Self);
+        Vector3 scale = new Vector3(transform.lossyScale.x, y, transform.lossyScale.z);
+        Collider[] colliders = Physics.OverlapSphere(f.position, y, stickableItems);
+        Destroy(go);
+
+        if (colliders.Length >= 1) {
+
+            Transform t = colliders[0].GetComponent<Transform>();
+            transform.SetParent(t);
+            t.GetComponent<BoxCollider>().enabled = false;
+            relativePos = new Vector3(0, 0, 0);
+            relativeOr = new Vector3(90, 0, 0);
+            transform.localPosition = relativePos;
+            transform.localEulerAngles = relativeOr;
+            transform.localScale = new Vector3(1, 1, 1);
+            canShoot = true;
+        }
+    }
+
+    private void Shoot()
     {
         GameObject[] bullets = new GameObject[4];
 
@@ -100,5 +131,4 @@ public class trap_bullet : MonoBehaviour
             bullets[i].GetComponent<Rigidbody>().AddForce(f);
         }
     }
-
 }
