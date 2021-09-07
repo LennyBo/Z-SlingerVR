538c5ef84d4fa61793e9dec9bce7ac996a158ff7
SuspicousDev <lenny.boegli@he-arc.ch>
2021-09-07 13:51:21 +0200

    Little bit of code clean up

--- Assets/Shop/UI/Scripts/ShopDisabler.cs
+++ Assets/Shop/UI/Scripts/ShopDisabler.cs
@@ -40,17 +40,9 @@ public class ShopDisabler : MonoBehaviour
         }
     }
 
-    private float oldTouchValue = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(actionReferenceOverride.action.ReadValue<float>());
         if (actionReferenceOverride.action.ReadValue<float>() == 1)
         {
             isUIOn = false;
